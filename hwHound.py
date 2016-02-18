af0ac33683f64f3d3a90f9b4cd452befac662aed
Yong <wynnsu@gmail.com>
2016-02-17 21:04:29 -0800

    Change login method
    Fake explorer log in instead of using cookies

--- hwHound.py
+++ hwHound.py
@@ -11,20 +11,14 @@ header={
    'Accept-Encoding': config['Headers']['Accept-Encoding'],
    'Accept-Language': config['Headers']['Accept-Language'],
    'Connection': config['Headers']['Connection'],
-   'Cookie': config['Headers']['Cookie'],
-   'Host': config['Headers']['Host'],
-   'Referer': config['Headers']['Referer'],
    'User-Agent': config['Headers']['User-Agent']
    }
-r=requests.get(config['Address']['Link'],headers=header)
-soup=BeautifulSoup(r.text)
-pattern=re.compile("/Home/GetFile/")
-list=[]
-for link in soup.find_all("a",{ "class" : "fancybox" }):
-    if(pattern.match(link.get('href'))):
-        list.append((link.get('href'),link.text))
-for link, name in list:
-    with open(config['Directory']['Path']+name,"wb") as f:
-        r=requests.get(url_base+link,headers=header,stream=True)
-        f.write(r.content)
-    print(name+" Complete.")
+r=requests.get('http://www.npu.edu/',headers=header)
+soup=BeautifulSoup(r.text,"html.parser")
+target=''
+for link in soup.find_all('a'):
+    if link.text=='Student Log-In':
+        target=link.get('href')
+r=requests.get(target,headers=header)
+print(r.headers)
+print(r.request.headers)
